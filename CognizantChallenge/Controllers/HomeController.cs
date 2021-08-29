a0530877c8582e1a6fed4c9b5f808d05fcce8dbb
Igor Zavorotny <zavor.igor@gmail.com>
2021-08-29 15:35:19 +0700

    Remove extra

--- CognizantChallenge/Controllers/HomeController.cs
+++ CognizantChallenge/Controllers/HomeController.cs
@@ -143,14 +143,6 @@ namespace CognizantChallenge.Controllers
                 Message= "",
             });
         }
-        
-        
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
 
         protected override void Dispose(bool disposing)
         {
